9e1e1d6d7c70aadab865435438a557df062f0e42
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-08-11 11:10:17 +0800

    update build file to rename students whose wiki names don't match student page names

--- build.js
+++ build.js
@@ -6,10 +6,22 @@ const config = {
   output: './json', 
 };
 
+const rename = {
+  'Louie Tan': 'Louie Tan Yi Jie', 
+  'Kent Nguyen': 'Nguyen Anh Quan', 
+};
+
 fs.readdirSync(config.input).forEach((f) => {
   const inFile = path.join(config.input, f); 
   const outFile = path.join(config.output, f); 
-  let groups = JSON.parse(fs.readFileSync(inFile, { encoding: 'utf-8' })); 
+  let groups = require('./' + inFile);
+  
+  groups.forEach((group) => {
+    group[1].forEach((n, i) => {
+  	  if (rename.hasOwnProperty(n))
+  	    group[1][i] = rename[n]; 
+    });
+  });
   
   fs.writeFileSync(outFile, JSON.stringify({ 
   	groups, 
